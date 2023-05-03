3fe1d9245b178e1ceb8f43df06d82c80cc86d2a8
IsmailAlamir <ismaeelalamir@gmail.com>
2023-05-03 19:27:13 +0000

    ADD controller func

--- src/main/java/com/example/shoppy/Domain/Cart.java
+++ src/main/java/com/example/shoppy/Domain/Cart.java
@@ -1,18 +1,35 @@
 package com.example.shoppy.Domain;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@RequiredArgsConstructor
 @Entity
 public class Cart {
 
+    @Setter (value = AccessLevel.NONE)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
+
+    @NonNull
     private String name;
+
+    @NonNull
     private String description;
+
+
     private BigDecimal totalCost;
+
+    @OneToMany(mappedBy = "cart")
+    Set<Item> items;
 
 }
--- src/main/java/com/example/shoppy/Domain/Item.java
+++ src/main/java/com/example/shoppy/Domain/Item.java
@@ -1,11 +1,32 @@
 package com.example.shoppy.Domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.*;
+
 import java.math.BigDecimal;
 
+@JsonIgnoreProperties({"cart"}) //to Ignore the error that cause infinite loop between cart and item in createNewItem method
+@Setter
+@Getter
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
 public class Item {
 
+    @Setter (value = AccessLevel.NONE)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NonNull
     private String name;
+
+    @NonNull
     private BigDecimal cost; // for money
 
+    @ManyToOne
+    Cart cart;  // the name "cart" should be same as in Item class
 
 }
